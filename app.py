1532cba28224b7d23f5d55521e021f71f09b94f0
fiso <fisnik_k.09@hotmail.com>
2025-03-27 00:38:05 +0100

    Hide Streamlit's built-in multi-page feature

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Immobilien Auktion Platforom",
+    page_title="Immobilien Auktionsplatforom",
     page_icon=None,
     layout="wide",
 )
